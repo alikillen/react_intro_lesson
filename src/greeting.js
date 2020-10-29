9f95bb0ab39718d620d7d1ea09d753e1aad52f00
alikillen <alisoneme1994@gmail.com>
2020-10-29 14:46:01 +1000

    first commit

---
+++ src/greeting.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Greeting = () => {
+  let name = "Flextrack"
+  return <h1>Hi {name}, its my first day with react!</h1>
+}
+
+export default Greeting
---
+++ src/message.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Message = () => {
+  let users = "favourite users"
+  return <p> This is a message for my {users}. Long live the {users}!</p>
+}
+
+export default Message
